9f5e25fbc01b59a3b5a5c8f887acd5de24955294
Owais-Khan-ICD2O1-01 <owaisali.khan@student.tdsb.on.ca>
2024-09-24 14:12:51 +0000

    Answer

--- PP1_6.py
+++ PP1_6.py
@@ -21,7 +21,7 @@ def q2():
 # Output the phrase "hello Mr. Kalisz have you seen my work yet?"
 
 def q3():
-  print ('"hello Mr. Kalisz have you seen my work yet?"')
+  print ("hello Mr. Kalisz have you seen my work yet?")
 
 # From input recieve two numbers (can be decimal fractions).  
 # Output their result multiplied together.  Then round down to the nearest whole number
